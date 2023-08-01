d5e8c00c29f7c365fc65adbc45560fe5766f54f2
imaaquibali <imaaquibali@gmail.com>
2023-08-01 21:52:57 +0530

    react portfolio

---
+++ src/content_option.js
@@ -0,0 +1,160 @@
+const logotext = "AAQUIB ALI";
+const meta = {
+  title: "Aaquib Ali",
+  description:
+    "Hello, I'm Aaquib Ali, a Developer from India. With a deep love for coding and a flair for crafting compelling content and designs, I strive to create impactful digital experiences and empower aspiring developers through community & mentorship.",
+};
+
+const introdata = {
+  title: "I’m Aaquib Ali",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I make designs",
+  },
+  description:
+    "Hello, I'm Aaquib Ali, a Developer from India. With a deep love for coding and a flair for crafting compelling content and designs, I strive to create impactful digital experiences and empower aspiring developers through community & mentorship.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "a bit about me",
+  aboutme:
+    "Hello, I'm Aaquib Ali, a Developer from India. With a deep love for coding and a flair for crafting compelling content and designs, I strive to create impactful digital experiences and empower aspiring developers through community & mentorship.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "UI/UX",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Website Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Content Writting",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "/Images/twr.png",
+    description:
+      "Discover, Engage, Elevate! Your one-stop destination for captivating articles, vibrant discussions, and limitless inspiration.",
+    link: "https://twr.vercel.app/",
+  },
+  {
+    img: "/Images/theversiton.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://theversiton.xyz/",
+  },
+  {
+    img: "/Images/springboyshostel.png",
+    description:
+      "An Open source sorta Spotify application where users can-•Fork other's Playlist•Create Issues on their albums•Send PRs for collaboration.",
+    link: "https://springboyshostel.vercel.app",
+  },
+  {
+    img: "/Images/portfolio.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://imaaquibali.vercel.app/",
+  },
+  {
+    img: "/Images/reactportfolio.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://aaquibaliportfolio.vercel.app",
+  },
+  {
+    img: "/Images/localboysshop.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://localboysshop.vercel.app/",
+  },
+
+  {
+    img: "/Images/githubtify.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "http://github.com/aliaaquib/githubtify/",
+  },
+  {
+    img: "/Images/buttonanimation.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://buttonanimation.vercel.app/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "imaaquibali@gmail.com",
+  YOUR_FONE: "+91 7419261380",
+  description:
+    "Hello, I'm Aaquib Ali, a Developer from India. With a deep love for coding and a flair for crafting compelling content and designs, I strive to create impactful digital experiences and empower aspiring developers through community & mentorship. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/aliaaquib",
+  linkedin: "https://linkedin.com/in/imaaquibali",
+  twitter: "https://twitter.com/imaaquibali",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
